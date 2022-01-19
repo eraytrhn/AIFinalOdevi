6a753ad0e902be7f1398dd303b49274dc429d58b
ercha <vxc67973@gmail.com>
2022-01-19 14:59:56 +0300

    Add project files.

---
+++ AIFinalOdevi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AIFinalOdevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
